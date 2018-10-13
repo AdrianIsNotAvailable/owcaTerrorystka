03ba9298a490bcab70b8c585959c3d6f41442e77
najs <04macciej@gmail.com>
2018-10-13 13:47:17 +0200

    Dodano zadanie domowe

--- wstep.py
+++ wstep.py
@@ -1,12 +1,10 @@
+#Zadanie nr.1
+
 ilekotow = input("podaj ile kotów chcesz mieć : ")
-#print('ilekotow jest : ', type(ilekotow) )
 
 try:
     ilekotow = int(ilekotow)
 except ValueError as owcaError:
     print(f'{owcaError}, program przyjmuje tylko liczby naturalne')
-
-
-
 
 input (ilekotow * " (=^.^=) ")
